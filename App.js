95d45f2221449da7285c2e3ae3e5333911ee9afb
Haseeb ur rehman <haseeb.ktk57@gmail.com>
2023-04-08 15:12:18 +0500

    product detail Screen

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import ProductScreen from "./src/Screens/ProductScreen";
-
+import ProductDetailScreen from "./src/Screens/ProductDetailScreen";
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <ProductScreen />
+      {/* <ProductScreen /> */}
+      <ProductDetailScreen />
     </View>
   );
 }
@@ -15,7 +16,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
