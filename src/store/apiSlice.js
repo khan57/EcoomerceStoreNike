dbb981cce6afa3f94e0c131671631989addb2cfc
Haseeb ur rehman <haseeb.ktk57@gmail.com>
2023-05-26 11:05:38 +0500

    products-api-integrated

--- src/store/apiSlice.js
+++ src/store/apiSlice.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseUrl = "http://192.168.1.103:3000";
+const baseUrl = "http://192.168.1.100:3000";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({ baseUrl }),
